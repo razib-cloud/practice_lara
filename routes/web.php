f638b454255d3d2a50733739ade63fb3fe2a4ff5
razib-cloud <razibidb61@gmail.com>
2025-01-29 13:18:07 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get("/razib", function() {
+return view("user");
+});
+
+
+Route::get('/users/{name?}', function($name=""){
+echo "My name is {$name}";
+
+});
+
+
+Route::get("/salman/{userName}", function($name){
+    return view("user", compact('name'));
+
+});
+
+
+Route::get("userlist", function(){
+
+    $users=[
+        ["id"=>1, "name"=>"hasan","address"=>"Dhaka"],
+        ["id"=>2, "name"=>"kabir","address"=>"Cumilla"],
+        ["id"=>3, "name"=>"dev","address"=>"Rangpur"],
+        ["id"=>4, "name"=>"tuli","address"=>"Lalbagh"],
+        ["id"=>5, "name"=>"tumpa","address"=>"Dhaka"],
+        ["id"=>6, "name"=>"kishori","address"=>"Gopalpur"],
+    ];
+
+    return view("userlist", compact('users'));
+
+
+
+});
+
+
+
